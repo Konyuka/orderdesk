22e27480414ddd9894f0cac0c9d2d010460c456f
konyuka <michaelsaiba84@gmail.com>
2023-05-16 15:15:21 +0300

    Code Refactoring

--- src/Integration/WooCommerce/ApiClient.php
+++ src/Integration/WooCommerce/ApiClient.php
@@ -3,51 +3,63 @@
 namespace OrderDesk\App\Integration\WooCommerce;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
-class ApiClient
+class ApiClient 
 {
-    public function __construct()
+    private $httpClient;
+    
+    public function __construct() 
     {
-        $this->http_client = new Client();
+        $this->httpClient = new Client();
     }
-
-    public function get_number_of_orders()
+    
+    public function getNumberOfOrders(): int
     {
         try {
-            $a = $this->http_client->request('GET', 'https://mysuperawesomestorewithstuffandthings.com/orders/list');
-        } catch (\Exception $e) {}
-
-        if ($a->getStatusCode() == 200) {
-            if (strpos($a->getBody(), '{') === 0) {
-                $data = json_decode($a->getBody());
-
-                if (isset($data['status']) && $data['status'] == 'success') {
-                    if ($data['items']) {
-                        return count($data['items']);
-                    }
-                }
-            }
+            $response = $this->httpClient->request('GET', 'https://mysuperawesomestorewithstuffandthings.com/orders/list');  
+        } catch (GuzzleException $e) {
+             return 0;   
+        }  
+        
+        $body = json_decode($response->getBody(), true);  
+        
+        if ($this->isValidResponse($body)) {
+            return count($body['items']);       
         }
-
-        return 0;
+        
+        return 0; 
     }
-
-    public function getShipments(int $orderId): array
+    
+    public function getShipments(int $orderId): array 
     {
         try {
-            $a = $this->http_client->request('GET', 'https://mysuperawesomestorewithstuffandthings.com/order/' . $orderId . '/shipments');
-        } catch (\Exception $e) {}
-
-        $shipments = json_decode($a->getBody(), true);
-
-        $shipment_info = [];
-
+           $response = $this->httpClient->request('GET', "https://mysuperawesomestorewithstuffandthings.com/order/$orderId/shipments");         
+        } catch (GuzzleException $e) {
+             return [];   
+        }        
+        
+        $shipments = json_decode($response->getBody(), true);
+        
+        return $this->extractTrackingNumbers($shipments);
+    } 
+    
+    private function isValidResponse($response): bool
+    {
+        return isset($response['status']) 
+             && $response['status'] === 'success';
+    }
+      
+    private function extractTrackingNumbers($shipments): array
+    {
+        $trackingNumbers = [];
+        
         foreach ($shipments as $shipment) {
             foreach ($shipment as $trackingNumber) {
-                $shipment_info[] = $trackingNumber;
-            }
+                $trackingNumbers[] = $trackingNumber;
+            }   
         }
-
-        return $shipment_info;
-    }
+          
+        return $trackingNumbers;        
+    } 
 }
